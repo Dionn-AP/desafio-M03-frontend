13a2fc0dce798c4bb88dba54abd7fb0ea5f78e9e
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-10 13:29:43 -0300

    primeiro commit do projeto com a criação das pastas e arquivos

---
+++ dindin/src/routes.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
+
+function MainRoutes() {
+
+
+
+  return (
+    <div className="container">
+    
+      
+
+    </div>
+  );
+}
+
+export default MainRoutes;
