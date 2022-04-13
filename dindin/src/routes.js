88b9a5615014be6963a386d59a6e5b6b546c1105
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-13 14:09:23 -0300

    importando as rotas das paginas e renderizando em root

--- dindin/src/routes.js
+++ dindin/src/routes.js
@@ -1,5 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
 import './index.css';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+
 
 function MainRoutes() {
 
@@ -8,7 +12,18 @@ function MainRoutes() {
   return (
     <div className="container">
     
-      
+      <Routes>
+        <Route path='/'>
+          <Route path='/' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+        </Route>
+        <Route path='/'>
+          <Route path='/sign-up' element={<SignUp />} />
+        </Route>
+        <Route path='/'>
+          <Route path='/home' element={<Home />} />
+        </Route>
+      </Routes>
 
     </div>
   );
