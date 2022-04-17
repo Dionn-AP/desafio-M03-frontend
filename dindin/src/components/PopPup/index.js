a266c2cf5c06a5ff8eef1c95e66b2987f69ff57a
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-17 19:41:17 -0300

    criando o componente Popup

---
+++ dindin/src/components/PopPup/index.js
@@ -0,0 +1,24 @@
+import './style.css';
+import IconPolygon from '../../assets/polygon.svg';
+
+
+function PopPup({deleteTransaction, setShowIcons, transaction}) {
+
+
+    return (
+        <div className='pop-pup'>
+            <img className='arrow-up' src={IconPolygon} alt='icon-arrow-up' />
+            Apagar item?
+            <div className='btn-yes-no'>
+                <button className='yes'
+                    onClick={() => deleteTransaction(transaction.id)}
+                >Sim</button>
+                <button className='no'
+                    onClick={() => setShowIcons(false)}
+                >Não</button>
+            </div>
+        </div>
+    )
+};
+
+export default PopPup;
