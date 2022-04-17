f608de940b11c748c1837124d335a4e5eeb5b2e2
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-04-17 19:45:35 -0300

    refatorando e adicionando a rota protegida

--- dindin/src/routes.js
+++ dindin/src/routes.js
@@ -1,13 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Outlet, Navigate } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import './index.css';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import { getItem } from './utils/storage';
 
+
+function ProtectedRoutes({ redirectTo }) {
+  const autorization = getItem('token');
+
+  return autorization ? <Outlet /> : <Navigate to={redirectTo} />
+  
+}
 
 function MainRoutes() {
-
-
 
   return (
     <div className="container">
@@ -20,9 +26,10 @@ function MainRoutes() {
         <Route path='/'>
           <Route path='/sign-up' element={<SignUp />} />
         </Route>
-        <Route path='/'>
+        <Route element={<ProtectedRoutes redirectTo='/' />}>
           <Route path='/home' element={<Home />} />
         </Route>
+        
       </Routes>
 
     </div>
